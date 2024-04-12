d0d4519eb7730b7b3a9d3e0262b8b8705f9cf19f
sai teja <sai1687teja123@gmail.com>
2024-04-12 15:53:41 +0530

    nltk

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ model = load_model('chatbot_model.h5')
 intents = json.loads(open('intents1.json').read())
 words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
-
+nltk.download('punkt')
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
